4a2bd67da48aee3c8d29017151a966fb89e9cb4d
saniasaeed01 <saniasaeed7023066@gmail.com>
2025-06-18 21:05:08 +0500

    Updated project files: added emergency service, modified UI, removed old documentation

--- android/app/src/main/kotlin/com/company/safewalkapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/company/safewalkapp/MainActivity.kt
@@ -3,3 +3,4 @@ package com.company.safewalkapp
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
+
--- android/app/src/main/kotlin/com/example/safety/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.safety
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
